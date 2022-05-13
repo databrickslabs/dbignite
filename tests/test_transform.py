a106fccfcee1559750ff97a99584ba96b6889442
kermany <amir.kermany@databricks.com>
2022-05-12 23:56:37 -0400

    debug workflows

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -20,15 +20,21 @@ BRANCH = re.sub(r'\W+', '', os.environ['BRANCH'])
 TEST_BUNDLE_PATH = '../sampledata/'
 TEST_DATABASE = f'test_{REPO}_{BRANCH}'
 
-
-
-
 # @unittest.skip("Not yet running as github action")
 class TestTransformers(TestCase):
   
   @classmethod
   def setUpClass(cls):
-    cls.spark = SparkSession.builder.appName("PyTest").getOrCreate()
+    self.spark = (SparkSession.builder.appName("myapp") \
+                      .config("spark.jars.packages", "io.delta:delta-core_2.12:1.1.0") \
+                      .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+                      .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+                      .config("spark.driver.extraJavaOptions", "-Dio.netty.tryReflectionSetAccessible=true") \
+                      .config("spark.executor.extraJavaOptions", "-Dio.netty.tryReflectionSetAccessible=true") \
+                      .master("local") \
+                      .getOrCreate())
+    self.spark.conf.set("spark.sql.shuffle.partitions", 1)
+    # cls.spark = SparkSession.builder.appName("PyTest").getOrCreate()
     cls.spark.sql(f'CREATE DATABASE IF NOT EXISTS {TEST_DATABASE}')
     cls.spark.catalog.setCurrentDatabase(TEST_DATABASE)
 
