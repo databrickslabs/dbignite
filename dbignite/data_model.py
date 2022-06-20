491c67286492988da0ac7a0264d32bfcb36fea01
kermany <amir.kermany@databricks.com>
2022-06-20 16:31:20 -0400

    code reformat

--- dbignite/data_model.py
+++ dbignite/data_model.py
@@ -12,14 +12,13 @@ from dbignite.schemas import ENTRY_SCHEMA
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 
-PERSON_TABLE = 'person'
-CONDITION_TABLE = 'condition'
-PROCEDURE_OCCURRENCE_TABLE = 'procedure_occurrence'
-ENCOUNTER_TABLE = 'encounter'
+PERSON_TABLE = "person"
+CONDITION_TABLE = "condition"
+PROCEDURE_OCCURRENCE_TABLE = "procedure_occurrence"
+ENCOUNTER_TABLE = "encounter"
 
 
 class DataModel(ABC):
-
     @abstractmethod
     def summary(self) -> DataFrame:
         ...
@@ -30,7 +29,6 @@ class DataModel(ABC):
 
 
 class FhirBundles(DataModel):
-
     def __init__(self, path: str):
         self.path = path
 
@@ -42,7 +40,6 @@ class FhirBundles(DataModel):
 
 
 class PersonDashboard(DataModel):
-
     def __init__(self, df: DataFrame):
         self.df = df
 
@@ -54,7 +51,6 @@ class PersonDashboard(DataModel):
 
 
 class OmopCdm(DataModel):
-
     def __init__(self, cdm_database: str, mapping_database: str = None):
         self.cdm_database = cdm_database
         self.mapping_database = mapping_database
@@ -66,31 +62,35 @@ class OmopCdm(DataModel):
         return (self.cdm_database, self.mapping_database)
 
 
-class Transformer():
-
+class Transformer:
     def __init__(self, spark):
         self.spark = spark
 
     def load_entries_df(self, path: str):
         entries_df = (
             self.spark.read.text(path, wholetext=True)
-                .select(explode(self._entry_json_strings('value')).alias('entry_json'))
-                .withColumn('entry', from_json('entry_json', schema=ENTRY_SCHEMA))
+                .select(explode(self._entry_json_strings("value")).alias("entry_json"))
+                .withColumn("entry", from_json("entry_json", schema=ENTRY_SCHEMA))
         ).cache()
-        return (entries_df)
+        return entries_df
 
     @staticmethod
     @udf(ArrayType(StringType()))  ## TODO change to pandas_udf
     def _entry_json_strings(value):
-        '''
+        """
         UDF takes raw text, returns the
         parsed struct and raw JSON.
-        '''
+        """
         bundle_json = json.loads(value)
-        return [json.dumps(e) for e in bundle_json['entry']]
+        return [json.dumps(e) for e in bundle_json["entry"]]
 
-    def fhir_bundles_to_omop_cdm(self, path: str, cdm_database: str, mapping_database: str = None,
-                                 overwrite: bool = True) -> OmopCdm:
+    def fhir_bundles_to_omop_cdm(
+            self,
+            path: str,
+            cdm_database: str,
+            mapping_database: str = None,
+            overwrite: bool = True,
+    ) -> OmopCdm:
 
         entries_df = self.load_entries_df(path)
 
@@ -99,31 +99,43 @@ class Transformer():
         procedure_occurrence_df = entries_df.transform(entries_to_procedure_occurrence)
         encounter_df = entries_df.transform(entries_to_encounter)
 
-        self.spark.sql(f'CREATE DATABASE IF NOT EXISTS {cdm_database}')
-        self.spark.sql(f'CREATE DATABASE IF NOT EXISTS {mapping_database}')
+        self.spark.sql(f"CREATE DATABASE IF NOT EXISTS {cdm_database}")
+        self.spark.sql(f"CREATE DATABASE IF NOT EXISTS {mapping_database}")
         self.spark.catalog.setCurrentDatabase(cdm_database)
 
-        print(f'created {cdm_database} and {mapping_database} databases')
+        print(f"created {cdm_database} and {mapping_database} databases")
 
         if overwrite:
             person_df.write.format("delta").mode("overwrite").saveAsTable(PERSON_TABLE)
-            condition_df.write.format("delta").mode("overwrite").saveAsTable(CONDITION_TABLE)
-            procedure_occurrence_df.write.format("delta").mode("overwrite").saveAsTable(PROCEDURE_OCCURRENCE_TABLE)
-            encounter_df.write.format("delta").mode("overwrite").saveAsTable(ENCOUNTER_TABLE)
-            print(f'created {PERSON_TABLE, CONDITION_TABLE, PROCEDURE_OCCURRENCE_TABLE} and {ENCOUNTER_TABLE} tables.')
+            condition_df.write.format("delta").mode("overwrite").saveAsTable(
+                CONDITION_TABLE
+            )
+            procedure_occurrence_df.write.format("delta").mode("overwrite").saveAsTable(
+                PROCEDURE_OCCURRENCE_TABLE
+            )
+            encounter_df.write.format("delta").mode("overwrite").saveAsTable(
+                ENCOUNTER_TABLE
+            )
+            print(
+                f"created {PERSON_TABLE, CONDITION_TABLE, PROCEDURE_OCCURRENCE_TABLE} and {ENCOUNTER_TABLE} tables."
+            )
 
         else:
             person_df.write.format("delta").saveAsTable(PERSON_TABLE)
             condition_df.write.format("delta").saveAsTable(CONDITION_TABLE)
-            procedure_occurrence_df.write.format("delta").saveAsTable(PROCEDURE_OCCURRENCE_TABLE)
+            procedure_occurrence_df.write.format("delta").saveAsTable(
+                PROCEDURE_OCCURRENCE_TABLE
+            )
             encounter_df.write.format("delta").saveAsTable(ENCOUNTER_TABLE)
-            print(f'updated {PERSON_TABLE, CONDITION_TABLE, PROCEDURE_OCCURRENCE_TABLE} and {ENCOUNTER_TABLE} tables.')
+            print(
+                f"updated {PERSON_TABLE, CONDITION_TABLE, PROCEDURE_OCCURRENCE_TABLE} and {ENCOUNTER_TABLE} tables."
+            )
 
         return OmopCdm(cdm_database, mapping_database)
 
     def omop_cdm_to_person_dashboard(self, omop_cdm: OmopCdm) -> PersonDashboard:
         cdm_database = omop_cdm.listDatabases()[0]
-        self.spark.sql(f'USE {cdm_database}')
+        self.spark.sql(f"USE {cdm_database}")
         person_df = self.spark.read.table(PERSON_TABLE)
         condition_df = self.spark.read.table(CONDITION_TABLE)
         procedure_occurrence_df = self.spark.read.table(PROCEDURE_OCCURRENCE_TABLE)
@@ -131,13 +143,14 @@ class Transformer():
         encounter_df = self.spark.read.table(ENCOUNTER_TABLE)
 
         condition_summary_df = condition_df.transform(summarize_condition)
-        procedure_occurrence_summary_df = procedure_occurrence_df.transform(summarize_procedure_occurrence)
+        procedure_occurrence_summary_df = procedure_occurrence_df.transform(
+            summarize_procedure_occurrence
+        )
 
         encounter_summary_df = encounter_df.transform(summarize_encounter)
 
         return PersonDashboard(
-            person_df
-                .join(condition_summary_df, 'person_id', 'left')
-                .join(procedure_occurrence_summary_df, 'person_id', 'left')
-                .join(encounter_summary_df, 'person_id', 'left')
+            person_df.join(condition_summary_df, "person_id", "left")
+                .join(procedure_occurrence_summary_df, "person_id", "left")
+                .join(encounter_summary_df, "person_id", "left")
         )
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,33 +2,50 @@ from pyspark.sql import SparkSession
 import os
 import re
 
-from dbignite.data_model import Transformer, PERSON_TABLE, CONDITION_TABLE, PROCEDURE_OCCURRENCE_TABLE, ENCOUNTER_TABLE
+from dbignite.data_model import (
+    Transformer,
+    PERSON_TABLE,
+    CONDITION_TABLE,
+    PROCEDURE_OCCURRENCE_TABLE,
+    ENCOUNTER_TABLE,
+)
 from dbignite.utils import *
 from dbignite.schemas import *
 
-REPO = os.environ.get('REPO', 'dbignite')
-BRANCH = re.sub(r'\W+', '', os.environ['BRANCH'])
-TEST_BUNDLE_PATH = './sampledata/'
-TEST_DATABASE = f'test_{REPO}_{BRANCH}'
+REPO = os.environ.get("REPO", "dbignite")
+BRANCH = re.sub(r"\W+", "", os.environ["BRANCH"])
+TEST_BUNDLE_PATH = "./sampledata/"
+TEST_DATABASE = f"test_{REPO}_{BRANCH}"
 
 
-class SparkTest():
+class SparkTest:
     ##
     ## Fixtures
     ##
     def setup_class(self) -> None:
-        self.spark = (SparkSession.builder.appName("myapp")
-                      .config("spark.jars.packages", "io.delta:delta-core_2.12:1.1.0")
-                      .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
-                      .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
-                      .config("spark.driver.extraJavaOptions", "-Dio.netty.tryReflectionSetAccessible=true")
-                      .config("spark.executor.extraJavaOptions", "-Dio.netty.tryReflectionSetAccessible=true")
-                      .master("local")
-                      .getOrCreate())
+        self.spark = (
+            SparkSession.builder.appName("dbignite")
+                .config("spark.jars.packages", "io.delta:delta-core_2.12:1.1.0")
+                .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+                .config(
+                "spark.sql.catalog.spark_catalog",
+                "org.apache.spark.sql.delta.catalog.DeltaCatalog",
+            )
+                .config(
+                "spark.driver.extraJavaOptions",
+                "-Dio.netty.tryReflectionSetAccessible=true",
+            )
+                .config(
+                "spark.executor.extraJavaOptions",
+                "-Dio.netty.tryReflectionSetAccessible=true",
+            )
+                .master("local")
+                .getOrCreate()
+        )
         self.spark.conf.set("spark.sql.shuffle.partitions", 1)
 
     def teardown_class(self) -> None:
-        self.spark.sql(f'DROP DATABASE IF EXISTS {TEST_DATABASE} CASCADE')
+        self.spark.sql(f"DROP DATABASE IF EXISTS {TEST_DATABASE} CASCADE")
 
     def assertSchemasEqual(self, schemaA: StructType, schemaB: StructType) -> None:
         """
@@ -64,7 +81,6 @@ class SparkTest():
 
 
 class TestUtils(SparkTest):
-
     def test_entries_to_person(self) -> None:
         entries_df = Transformer(self.spark).load_entries_df(TEST_BUNDLE_PATH)
         person_df = entries_df.transform(entries_to_person)
@@ -81,7 +97,9 @@ class TestUtils(SparkTest):
         entries_df = Transformer(self.spark).load_entries_df(TEST_BUNDLE_PATH)
         procedure_occurrence_df = entries_df.transform(entries_to_procedure_occurrence)
         assert procedure_occurrence_df.count() == 119
-        self.assertSchemasEqual(procedure_occurrence_df.schema, PROCEDURE_OCCURRENCE_SCHEMA)
+        self.assertSchemasEqual(
+            procedure_occurrence_df.schema, PROCEDURE_OCCURRENCE_SCHEMA
+        )
 
     def test_entries_to_encounter(self) -> None:
         entries_df = Transformer(self.spark).load_entries_df(TEST_BUNDLE_PATH)
@@ -91,49 +109,85 @@ class TestUtils(SparkTest):
 
     def test_summarize_condition(self) -> None:
         entries_df = Transformer(spark).load_entries_df(TEST_BUNDLE_PATH)
-        summarize_condition_df = entries_df.transform(entries_to_condition).transform(summarize_condition)
+        summarize_condition_df = entries_df.transform(entries_to_condition).transform(
+            summarize_condition
+        )
         summarize_condition_first = summarize_condition_df.first()
         assert summarize_condition_df.count() == 3
-        assert summarize_condition_first.person_id == '4a0bf980-a2c9-36d6-da55-14d7aa5a85d9'
-        assert summarize_condition_first.conditions[0].condition_occurrence_id == 'a6425765-fb3a-3b1a-161b-4b5ad0fe0495'
-        assert summarize_condition_first.conditions[0].visit_occurrence_id == 'a6118cdf-24ea-94fe-6a6e-30e91c841e5a'
-        assert summarize_condition_first.conditions[0].condition_status == 'Hypertension'
+        assert (
+                summarize_condition_first.person_id
+                == "4a0bf980-a2c9-36d6-da55-14d7aa5a85d9"
+        )
+        assert (
+                summarize_condition_first.conditions[0].condition_occurrence_id
+                == "a6425765-fb3a-3b1a-161b-4b5ad0fe0495"
+        )
+        assert (
+                summarize_condition_first.conditions[0].visit_occurrence_id
+                == "a6118cdf-24ea-94fe-6a6e-30e91c841e5a"
+        )
+        assert (
+                summarize_condition_first.conditions[0].condition_status == "Hypertension"
+        )
 
     def test_summarize_procedure_occurrence(self) -> None:
         entries_df = Transformer(spark).load_entries_df(TEST_BUNDLE_PATH)
-        summarize_procedure_occurrence_df = entries_df.transform(entries_to_procedure_occurrence).transform(
-            summarize_procedure_occurrence)
+        summarize_procedure_occurrence_df = entries_df.transform(
+            entries_to_procedure_occurrence
+        ).transform(summarize_procedure_occurrence)
         summarize_procedure_occurrence_first = summarize_procedure_occurrence_df.first()
         assert summarize_procedure_occurrence_df.count() == 3
-        assert summarize_procedure_occurrence_first.person_id == '4a0bf980-a2c9-36d6-da55-14d7aa5a85d9'
-        assert summarize_procedure_occurrence_first.procedure_occurrences[
-                   0].procedure_occurrence_id == '562ad62b-0671-c044-8dd1-4f60ad010bec'
+        assert (
+                summarize_procedure_occurrence_first.person_id
+                == "4a0bf980-a2c9-36d6-da55-14d7aa5a85d9"
+        )
+        assert (
+                summarize_procedure_occurrence_first.procedure_occurrences[
+                    0
+                ].procedure_occurrence_id
+                == "562ad62b-0671-c044-8dd1-4f60ad010bec"
+        )
         assert len(summarize_procedure_occurrence_first) == 2
 
     def test_summarize_encounter(self) -> None:
         entries_df = Transformer(spark).load_entries_df(TEST_BUNDLE_PATH)
-        summarize_encounter_df = entries_df.transform(entries_to_encounter).transform(summarize_encounter)
+        summarize_encounter_df = entries_df.transform(entries_to_encounter).transform(
+            summarize_encounter
+        )
         summarize_encounter_first = summarize_encounter_df.first()
         assert summarize_encounter_df.count() == 3
         assert len(summarize_encounter_first.encounters) == 76
-        assert summarize_encounter_first.person_id == '4a0bf980-a2c9-36d6-da55-14d7aa5a85d9'
-        assert summarize_encounter_first.encounters[1].encounter_id == '83075051-3b84-b92a-cefb-66497ee9d602'
-        assert summarize_encounter_first.encounters[1].location[
-                   0].location.display == 'BETH ISRAEL DEACONESS HOSPITAL-MILTON INC'
+        assert (
+                summarize_encounter_first.person_id
+                == "4a0bf980-a2c9-36d6-da55-14d7aa5a85d9"
+        )
+        assert (
+                summarize_encounter_first.encounters[1].encounter_id
+                == "83075051-3b84-b92a-cefb-66497ee9d602"
+        )
+        assert (
+                summarize_encounter_first.encounters[1].location[0].location.display
+                == "BETH ISRAEL DEACONESS HOSPITAL-MILTON INC"
+        )
 
 
 # summarize_encounter
 
-class TestTransformers(SparkTest):
 
+class TestTransformers(SparkTest):
     def test_load_entries_df(self) -> None:
         entries_df = Transformer(self.spark).load_entries_df(TEST_BUNDLE_PATH)
         assert entries_df.count() == 1872
         self.assertSchemasEqual(entries_df.schema, JSON_ENTRY_SCHEMA)
 
     def test_fhir_bundles_to_omop_cdm(self) -> None:
-        omop_cdm = Transformer(self.spark).fhir_bundles_to_omop_cdm(TEST_BUNDLE_PATH, TEST_DATABASE, None, True)
-        tables = [t.tableName for t in self.spark.sql(f"SHOW TABLES FROM {TEST_DATABASE}").collect()]
+        omop_cdm = Transformer(self.spark).fhir_bundles_to_omop_cdm(
+            TEST_BUNDLE_PATH, TEST_DATABASE, None, True
+        )
+        tables = [
+            t.tableName
+            for t in self.spark.sql(f"SHOW TABLES FROM {TEST_DATABASE}").collect()
+        ]
 
         assert TEST_DATABASE in omop_cdm.listDatabases()
         assert PERSON_TABLE in tables
@@ -146,12 +200,16 @@ class TestTransformers(SparkTest):
     # @unittest.skip("Not yet running as github action")
     def test_omop_cdm_to_person_dashboard(self) -> None:
         transformer = Transformer(self.spark)
-        omop_cdm = transformer.fhir_bundles_to_omop_cdm(TEST_BUNDLE_PATH, TEST_DATABASE, None, True)
+        omop_cdm = transformer.fhir_bundles_to_omop_cdm(
+            TEST_BUNDLE_PATH, TEST_DATABASE, None, True
+        )
         person_dashboard = transformer.omop_cdm_to_person_dashboard(omop_cdm).summary()
-        self.assertSchemasEqual(CONDITION_SUMMARY_SCHEMA, person_dashboard.select('conditions').schema)
+        self.assertSchemasEqual(
+            CONDITION_SUMMARY_SCHEMA, person_dashboard.select("conditions").schema
+        )
 
 
 ## MAIN
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
 # %%
