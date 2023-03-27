1a31f403707da383205f6ee822f03282c71211d7
Aaron Zavora <aaron.zavora@databricks.com>
2023-03-27 12:30:24 -0400

    using unittests for simplicity

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,5 @@
 from pyspark.sql import SparkSession
-import os
-import re
-import pytest
+import os, re, unittest
 
 from chispa import *
 from chispa.schema_comparer import *
@@ -11,60 +9,71 @@ from dbignite.utils import *
 from dbignite.schemas import *
 
 REPO = os.environ.get("REPO", "dbignite")
-BRANCH = re.sub(r"\W+", "", os.environ.get("BRANCH", 'local_test'))
-
 TEST_BUNDLE_PATH = "./sampledata/"
-TEST_DATABASE = f"test_{REPO}_{BRANCH}"
+TEST_DATABASE = f"test_{REPO}"
 
 
-@pytest.fixture
-def get_entries_df(spark_session):
-    entries_df = FhirBundlesToCdm(spark_session).loadEntries(TEST_BUNDLE_PATH)
-    return(entries_df)
+class TestUtils(unittest.TestCase):
+    def setUp(self):
+        self.spark = (
+            SparkSession.builder
+            .appName("myapp")
+            .config("spark.driver.bindAddress", "127.0.0.1")
+            .getOrCreate()
+        )
+        self.fhirBundles = FhirBundles(path=TEST_BUNDLE_PATH)
+        self.cdmModel = cdm_model=OmopCdm(TEST_DATABASE)
+        self.entriesDF = self.fhirBundles.loadEntries()
 
-@pytest.fixture
-def fhir_model():
-    fhir_model=FhirBundles(TEST_BUNDLE_PATH)
-    return(fhir_model)
+    def tearDown(self):
+        self.spark.stop()
 
-@pytest.fixture
-def cdm_model():
-    cdm_model=OmopCdm(TEST_DATABASE)
-    return(cdm_model)
-
-class TestUtils:
-
-    def test_entries_to_person(self,get_entries_df) -> None:
-        person_df = entries_to_person(get_entries_df)
+    def test_entries_to_person(self):
+        person_df = entries_to_person(self.entriesDF)
         assert person_df.count() == 3
         assert_schema_equality(person_df.schema, PERSON_SCHEMA, ignore_nullable=True)
 
 
-    def test_entries_to_condition(self, get_entries_df) -> None:
-        condition_df = entries_to_condition(get_entries_df)
+    def test_entries_to_condition(self):
+        condition_df = entries_to_condition(self.entriesDF)
         assert condition_df.count() == 103
         assert_schema_equality(condition_df.schema, CONDITION_SCHEMA,ignore_nullable=True)
 
-    def test_entries_to_procedure_occurrence(self, get_entries_df) -> None:
-        procedure_occurrence_df = entries_to_procedure_occurrence(get_entries_df)
+    def test_entries_to_procedure_occurrence(self):
+        procedure_occurrence_df = entries_to_procedure_occurrence(self.entriesDF)
         assert procedure_occurrence_df.count() == 119
         assert_schema_equality(procedure_occurrence_df.schema, PROCEDURE_OCCURRENCE_SCHEMA, ignore_nullable=True)
 
-    def test_entries_to_encounter(self, get_entries_df) -> None:
-        encounter_df = entries_to_encounter(get_entries_df)
+    def test_entries_to_encounter(self):
+        encounter_df = entries_to_encounter(self.entriesDF)
         assert encounter_df.count() == 128
         assert_schema_equality(encounter_df.schema, ENCOUNTER_SCHEMA,ignore_nullable=True)
 
 
-class TestTransformers:
+class TestTransformers(unittest.TestCase):
 
-    def test_loadEntries(self, get_entries_df) -> None:
-        assert get_entries_df.count() == 1872
-        assert_schema_equality(get_entries_df.schema, JSON_ENTRY_SCHEMA,ignore_nullable=True)
+    def setUp(self):
+        self.spark = (
+            SparkSession.builder
+            .appName("myapp")
+            .config("spark.driver.bindAddress", "127.0.0.1")
+            .getOrCreate()
+        )
+        self.fhirModel = FhirBundles(TEST_BUNDLE_PATH)
+        self.cdmModel = cdm_model=OmopCdm(TEST_DATABASE)
+        self.entriesDF = self.fhirModel.loadEntries()
 
-    def test_fhir_bundles_to_omop_cdm(self, spark_session,fhir_model,cdm_model) -> None:
-        FhirBundlesToCdm(spark_session).transform(fhir_model, cdm_model, True)
-        tables = [t.tableName for t in spark_session.sql(f"SHOW TABLES FROM {TEST_DATABASE}").collect()]
+    def tearDown(self):
+        self.spark.stop()
+
+
+    def test_loadEntries(self):
+        assert self.entriesDF.count() == 1872
+        assert_schema_equality(self.entriesDF.schema, JSON_ENTRY_SCHEMA,ignore_nullable=True)
+
+    def test_fhir_bundles_to_omop_cdm(self):
+        FhirBundlesToCdm(self.spark).transform(fhirModel, cdmModel, True)
+        tables = [t.tableName for t in self.spark.sql(f"SHOW TABLES FROM {TEST_DATABASE}").collect()]
 
         assert TEST_DATABASE in cdm_model.listDatabases()
         assert PERSON_TABLE in tables
@@ -72,13 +81,13 @@ class TestTransformers:
         assert PROCEDURE_OCCURRENCE_TABLE in tables
         assert ENCOUNTER_TABLE in tables
 
-        assert spark_session.table(f"{TEST_DATABASE}.person").count() == 3
+        assert self.spark.table(f"{TEST_DATABASE}.person").count() == 3
 
-    def test_omop_cdm_to_person_dashboard(self, spark_session, fhir_model, cdm_model) -> None:
-        transformer = CdmToPersonDashboard(spark_session)
+    def test_omop_cdm_to_person_dashboard(self):
+        transformer = CdmToPersonDashboard(self.spark)
         person_dash_model=PersonDashboard()
         
-        FhirBundlesToCdm(spark_session).transform(fhir_model, cdm_model, True)
-        CdmToPersonDashboard(spark_session).transform(cdm_model,person_dash_model)
+        FhirBundlesToCdm(self.spark).transform(fhirModel, cdmModel, True)
+        CdmToPersonDashboard(self.spark).transform(cdmModel,person_dash_model)
         person_dashboard_df=person_dash_model.summary()
         assert_schema_equality(CONDITION_SUMMARY_SCHEMA, person_dashboard_df.select('conditions').schema, ignore_nullable=True)
