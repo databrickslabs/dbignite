5d4c489619ffd69c2189e0e5ce7e57d67fb1067e
willsmithDB <willsmithDB>
2023-06-21 21:37:12 +0000

    Updated Pip installation for new build of dbignite

--- notebooks/fhir-mapping-demo.py
+++ notebooks/fhir-mapping-demo.py
@@ -1,38 +1,10 @@
 # Databricks notebook source
-# import dbignite.fhir_mapping_model
+pip install git+https://github.com/databricks-industry-solutions/dbignite.git@feature-FHIR-schema-dbignite-HLSSA-289
+
 
 # COMMAND ----------
 
-# DBTITLE 1,Cannot Import Currently So Copied Class 
-import os, sys, json
-from pyspark.sql.types import *
-
-class fhirSchemaModel():
-    def __init__(self, mapping = None):
-        self.mapping = self.createFHIRMapping()
-
-    def createFHIRMapping(self) -> dict[str, str]:
-      schema_path = "../schemas"
-      schema_dir = os.listdir( schema_path )
-
-      resource_map = {}
-
-      for file in schema_dir:
-        with open("../schemas/" + file) as schema_file:
-          schema_struct = StructType.fromJson(json.load(schema_file))
-          resource_map[file.replace('.json', '')] = schema_struct
-      return resource_map
-    
-    def resource(self, resourceName: str) -> str:
-      return self.mapping[resourceName]
-    
-    # Adding due to ambiguity on resource / schema retrieval call 
-    def schema(self, resourceName: str) -> str:
-      return self.mapping[resourceName]
-
-    #debugging    
-    def debug_print_keys(self):
-      print(self.mapping.keys())
+from dbignite.fhir_mapping_model import *
 
 # COMMAND ----------
 
