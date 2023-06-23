56f055355d4f7295ddac405359debabd7eb88a75
willsmithDB <willsmithDB>
2023-06-23 18:59:09 +0000

    Updated Variable Names

--- dbignite/fhir_mapping_model.py
+++ dbignite/fhir_mapping_model.py
@@ -3,19 +3,16 @@ from pyspark.sql.types import *
 # from dbignite.fhir_dict_object import *
 
 class fhirSchemaModel():
-    def __init__(self, mapping = None):
-        self.mapping = load_from_dir()
-    
-    def load_from_dir():
-      return {x[:-5]: json.load(open("../schemas/" + x, "r")) for x in os.listdir("../schemas")}
+    def __init__(self, fhir_resource_map = None):
+        self.fhir_resource_map = {x[:-5]: json.load(open("../schemas/" + x, "r")) for x in os.listdir("../schemas")}
     
     def resource(self, resourceName: str) -> str:
-      return self.mapping[resourceName]
+      return self.fhir_resource_map[resourceName]
     
     # Adding due to ambiguity on resource / schema retrieval call 
     def schema(self, resourceName: str) -> str:
-      return self.mapping[resourceName]
+      return self.fhir_resource_map[resourceName]
 
     # Debugging    
     def debug_print_keys(self):
-      print(self.mapping.keys())
+      print(self.fhir_resource_map.keys())
