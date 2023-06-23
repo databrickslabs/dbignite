3d9946153c59a0e05cf13106e1295a404f590f18
willsmithDB <willsmithDB>
2023-06-23 19:56:08 +0000

    String for PosixPath

--- dbignite/fhir_mapping_model.py
+++ dbignite/fhir_mapping_model.py
@@ -7,7 +7,7 @@ class fhirSchemaModel():
     def __init__(self, fhir_resource_map = None):
         
         # Quicker runtime with package_data so there is no reliance on directory structure 
-        self.fhir_resource_map = { x[:-5] : StructType.fromJson(json.load(open(x, "r"))) for x in list(files("schemas").iterdir())}
+        self.fhir_resource_map = { str(x)[:-5] : StructType.fromJson(json.load(open(x, "r"))) for x in list(files("schemas").iterdir())}
 
         # Quickest runtime from a single python file with struct information (0.1 seconds)
         # self.python_struct_fhir_resource_map = fhir_dict_map
