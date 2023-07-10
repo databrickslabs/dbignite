deb21bd046665edfabe8e0503412778dc0e82db0
willsmithDB <willsmithDB>
2023-07-10 17:28:55 +0000

    Updated Init logic to be class methods

--- dbignite/fhir_mapping_model.py
+++ dbignite/fhir_mapping_model.py
@@ -7,34 +7,45 @@ class FhirSchemaModel():
     #
     # Class that manages access to FHIR resourceType ->  Spark Schema mapping
     #
-    def __init__(self, fhir_resource_map = None, subset = None):
+    def __init__(self, fhir_resource_map = None):
 
         # Create mapping with ALL FHIR Resources 
-        if fhir_resource_map is None and subset is None:
-            print("Creating FHIR Resource Mapping with ALL Resources")
-            self.fhir_resource_map = { str(x).rsplit('/',1)[1][:-5] : StructType.fromJson(json.load(open(x, "r"))) for x in list(files("schemas").iterdir())}
-        
-        # Create mapping with FHIR US Core 
-        elif fhir_resource_map is None and subset == "UScore":
-            print("Creating FHIR Resource Mapping with US Core Resources")
-            us_core_resources = ["AllergyIntolerance", "Bundle", "CarePlan", "CareTeam", "Condition", "Coverage", "Device", "DiagnosticReport", "DocumentReference", "Encounter", "Goal", "Immunization", "Location", "Medication", "MedicationDispense", "MedicationRequest", "Observation", "Organization", "Patient", "Practitioner", "Procedure", "Provenance", "Questionnaire", "QuestionnaireResponse", "RelatedPerson", "ServiceRequest", "Specimen"]
-            self.fhir_resource_map = { x : StructType.fromJson(json.load(open(files('schemas').joinpath(x + ".json"), "r"))) for x in us_core_resources}
-        
-        # Set the mapping with provided parameter if the typing is correct
-        elif fhir_resource_map and type(fhir_resource_map) is dict:
-          print("Creating FHIR Resource Mapping with Provided Dictionary")
-          self.fhir_resource_map = fhir_resource_map
-
-        # Fail gracefully 
+        if fhir_resource_map is None:
+          self.fhir_resource_map = { str(x).rsplit('/',1)[1][:-5] : StructType.fromJson(json.load(open(x, "r"))) for x in list(files("schemas").iterdir())}
         else:
-          raise Exception("Provided fhir_resource_map is not a valid. Please provide a dictionary or omit the fhir_resource_map parameter. You may have a subset=UScore to load the US Core Subset")
-
+          self.fhir_resource_map = fhir_resource_map
+          
     #
     # Given a resourceName, return the spark schema representation
     #
     def schema(self, resourceName):
         return self.fhir_resource_map[resourceName]
+      
+    #
+    # Load all FHIR resources into one dictionary 
+    # 
 
+    @classmethod
+    def all_fhir_resource_mapping(cls):
+        return { str(x).rsplit('/',1)[1][:-5] : StructType.fromJson(json.load(open(x, "r"))) for x in list(files("schemas").iterdir())}
+      
+    #
+    # Load US Core FHIR resources into one dictionary 
+    # 
+
+    @classmethod
+    def us_core_fhir_resource_mapping(cls):
+        us_core_resources = ["AllergyIntolerance", "Bundle", "CarePlan", "CareTeam", "Condition", "Coverage", "Device", "DiagnosticReport", "DocumentReference", "Encounter", "Goal", "Immunization", "Location", "Medication", "MedicationDispense", "MedicationRequest", "Observation", "Organization", "Patient", "Practitioner", "Procedure", "Provenance", "Questionnaire", "QuestionnaireResponse", "RelatedPerson", "ServiceRequest", "Specimen"]
+        return { x : StructType.fromJson(json.load(open(files('schemas').joinpath(x + ".json"), "r"))) for x in us_core_resources}
+      
+    #
+    # Load supplied subset of FHIR resources into one dictionary 
+    # 
+
+    @classmethod
+    def custom_fhir_resource_mapping(cls, resource_list):
+        return { x : StructType.fromJson(json.load(open(files('schemas').joinpath(x + ".json"), "r"))) for x in resource_list}
+      
     #
     # Return all keys of FHIR Resource References
     #
