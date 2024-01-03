0d6245854e78b4d80af96b2082511067a301dac0
Aaron Z <aaron.zavora@databricks.com>
2024-01-03 13:46:17 -0500

    removing id and timestamp

--- dbignite/readers.py
+++ dbignite/readers.py
@@ -3,14 +3,20 @@ from pyspark.sql.functions import col
 
 from .fhir_resource import FhirResource
 
-def read_from_directory(
-    path: str, spark: SparkSession = SparkSession.getActiveSession()
-) -> FhirResource:
+#
+# default of FHIR bundle reads
+#
+def read_from_directory(path: str, spark: SparkSession = SparkSession.getActiveSession()) -> FhirResource:
     raw_data = spark.read.text(path, wholetext=True).select(
         col("value").alias("resource")
     )
     return FhirResource.from_raw_resource(raw_data)
 
+#
+# ndjson FHIR data
+#
+def read_from_directory_ndjson(path: str, spark: SparkSession = SparkSession.getActiveSession()) -> FhirResource:
+    pass
 
 def read_from_stream() -> FhirResource:
     # TODO
